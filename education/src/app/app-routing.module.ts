b0d533047e1de910e85bbcbba87780d093cdf200
Lee <132388212+leesamatham@users.noreply.github.com>
2023-05-12 14:41:29 -0400

    Routing files are added

--- education/src/app/app-routing.module.ts
+++ education/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { RegisterComponent } from './register/register.component';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'services',
+    component:ServicesComponent
+  },
+  {
+    path:'about',
+    component:AboutusComponent
+  },
+  {
+    path:'contact',
+    component:ContactusComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  },
+  {
+    path:'signin',
+    component:SigninComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- education/src/app/app.module.ts
+++ education/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { RegisterComponent } from './register/register.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent,
+    ContactusComponent,
+    AboutusComponent,
+    RegisterComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
