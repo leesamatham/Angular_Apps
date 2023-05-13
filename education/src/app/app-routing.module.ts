b992f5c2acd4b6943f38b7fc4971ee22a6e15be1
Lee <132388212+leesamatham@users.noreply.github.com>
2023-05-12 21:29:50 -0400

    Home/Courses/Aboutus/Register pages added

--- education/src/app/app-routing.module.ts
+++ education/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { ContactusComponent } from './contactus/contactus.component';
 import { RegisterComponent } from './register/register.component';
-import { SigninComponent } from './signin/signin.component';
 import { CoursesComponent } from './courses/courses.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 const routes: Routes = [
   {
@@ -20,18 +18,12 @@ const routes: Routes = [
     path:'about',
     component:AboutusComponent
   },
-  {
-    path:'contact',
-    component:ContactusComponent
-  },
+  
   {
     path:'register',
     component:RegisterComponent
   },
-  {
-    path:'signin',
-    component:SigninComponent
-  }
+  
 ];
 
 @NgModule({
--- education/src/app/app.module.ts
+++ education/src/app/app.module.ts
@@ -4,20 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ContactusComponent } from './contactus/contactus.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { RegisterComponent } from './register/register.component';
-import { SigninComponent } from './signin/signin.component';
 import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactusComponent,
     AboutusComponent,
     RegisterComponent,
-    SigninComponent,
     CoursesComponent
   ],
   imports: [
